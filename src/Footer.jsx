695aef76d401f134d5c3f781b50d691ccd6218a4
SHANGEETH <shanshangeeth@gmail.com>
2022-11-07 13:12:09 +0530

    nn

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <>
+      <section id="footer">
+        <footer class="py-5 bg-dark">
+          <div class="container">
+            <p class="m-0 text-center text-white">
+              Copyright ©2021 Powered By DowntownShopping.com{" "}
+            </p>
+          </div>
+        </footer>
+      </section>
+    </>
+  );
+}
+export default Footer;
---
+++ src/Nav.js
@@ -0,0 +1,52 @@
+function Nav() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-light">
+        <div className="container-fluid ">
+          <a className="navbar-brand text-warning fw-bold fs-3" href="#">
+            DOWNTOWN
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse text-light"
+            id="navbarTogglerDemo01"
+          >
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link fs-6 text-dark "
+                  aria-current="page"
+                  href="#home"
+                >
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link fs-6  text-dark" href="#card">
+                  COLLECTION
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link fs-6 text-dark" href="#footer">
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
